4e1a7d02f8669169e06c7a2edc8a85e56bb6466a
Sander Brokelman <sander@brokelman.nl>
2016-06-07 21:42:46 +0200

    Worst AI in history of mankind

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -3,12 +3,17 @@ angular.module('app.controllers').controller('mainController', function ($scope,
 	$scope.init = function() {
 		$scope.game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: $scope.preload, create: $scope.create, update: $scope.update, render: $scope.render });
 
-		$scope.bodies = new Array();
-
 		$scope.bodySize = 10;
-		$scope.position =  {x: 80, y: 600};
-		$scope.direction = "up";
+
+		// localplayer properties
+		$scope.bodies = new Array();
+		$scope.position =  {x: 350, y: 250};
+		$scope.direction = "left";
 	
+		$scope.players = new Array();
+		$scope.players.push({name: 'Player 2', bodies: new Array(), direction: "left", position: {x: 350, y: 350}, color:"#5bffff"});
+		$scope.players.push({name: 'Player 3', bodies: new Array(), direction: "right", position:  {x: 450, y: 250}, color:"#FF9F1E" });
+		$scope.players.push({name: 'Player 4', bodies: new Array(), direction: "right", position:  {x: 450, y: 350}, color:"#4ea683" });
 	},
 
 	$scope.preload = function() {
@@ -51,11 +56,12 @@ angular.module('app.controllers').controller('mainController', function ($scope,
 
 	$scope.update = function() {
 		
-		// Update local player
+		
 		if ($scope.game.time.now - $scope.timeCheck > 100) {
 
 			$scope.timeCheck = $scope.game.time.now;
 
+			// Update local player
 			if($scope.direction == "up") {
 				$scope.position.y -= $scope.bodySize;
 			}
@@ -69,25 +75,62 @@ angular.module('app.controllers').controller('mainController', function ($scope,
 				$scope.position.x += $scope.bodySize;
 			}
 
-			// TODO: We should get new positions even from ourself from the server
+			// TODO: We should get new positions from the server. even for the localplayer
 			$scope.bodies.push(new Phaser.Rectangle($scope.position.x, $scope.position.y, $scope.bodySize, $scope.bodySize));
+
+			// update other players
+			for (var i = 0; i < $scope.players.length; i++) {
+				var player = $scope.players[i];
+
+				// random direction
+				var r = Math.random(0, 4) * 100;
+				if(r > 0 && r < 25) { player.direction = "up"; }
+				if(r > 25 && r < 50) { player.direction = "down"; }
+				if(r > 50 && r < 75) { player.direction = "left"; }
+				if(r > 75 && r < 100) { player.direction = "right"; }
+
+				if(player.direction == "up") {
+					player.position.y -= $scope.bodySize;
+				}
+				else if(player.direction == "down") {
+					player.position.y += $scope.bodySize;
+				}
+				else if(player.direction == "left") {
+					player.position.x -= $scope.bodySize;
+				}
+				else if(player.direction == "right") {
+					player.position.x += $scope.bodySize;
+				}
+
+				player.bodies.push(new Phaser.Rectangle(player.position.x, player.position.y, $scope.bodySize, $scope.bodySize));
+				
+			}
 		}
-
-
 	}
 
 	$scope.render = function() {
-		//console.log('render');
-
-		// draw body
+		// draw localplayer's body
 		for (var i = $scope.bodies.length - 2; i >= 0; i--) {
 			var body = $scope.bodies[i];
 
 			$scope.game.debug.geom(body, '#ff00a2');
 		};
-		
-		//draw head
+
+		//draw localplayer's head
 		var head = $scope.bodies[$scope.bodies.length -1 ];
 		$scope.game.debug.geom(head, '#ffffff');
+
+		// draw other players
+		for (var i = 0; i < $scope.players.length; i++) {
+			var player = $scope.players[i];
+
+			for (var j = 0; j < player.bodies.length - 1; j++) {
+				var body = player.bodies[j];
+				$scope.game.debug.geom(body, player.color);
+			}
+
+			var head = player.bodies[player.bodies.length -1 ];
+			$scope.game.debug.geom(head, '#ffffff');
+		}
 	}
 });
