95adaec24b4bed41e2130c0942eb523c7e7f34cc
Sander Brokelman <sander@brokelman.nl>
2016-06-07 20:43:58 +0200

    basic movement and input control

--- public/javascripts/controllers/main.js
+++ public/javascripts/controllers/main.js
@@ -3,6 +3,11 @@ angular.module('app.controllers').controller('mainController', function ($scope,
 	$scope.init = function() {
 		$scope.game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: $scope.preload, create: $scope.create, update: $scope.update, render: $scope.render });
 		$scope.bodies = new Array();
+
+		$scope.bodySize = 10;
+		$scope.position =  {x: 80, y: 600};
+		$scope.direction = "up";
+	
 	},
 
 	$scope.preload = function() {
@@ -11,15 +16,60 @@ angular.module('app.controllers').controller('mainController', function ($scope,
 
 	$scope.create = function() {
 		console.log('create');
-		$scope.game.physics.startSystem(Phaser.Physics.ARCADE);
+		
     	$scope.game.world.setBounds(0, 0, 800, 600);
+    	$scope.game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    	$scope.bodies.push(new Phaser.Rectangle(50, 50, 50, 50));
-    	$scope.bodies.push(new Phaser.Rectangle(250, 250, 50, 50));
+    	$scope.timeCheck = $scope.game.time.now;;
+
+
+    	// Setup input
+    	$scope.up = $scope.game.input.keyboard.addKey(Phaser.Keyboard.W);
+    	$scope.down = $scope.game.input.keyboard.addKey(Phaser.Keyboard.S);
+    	$scope.left = $scope.game.input.keyboard.addKey(Phaser.Keyboard.A);
+    	$scope.right = $scope.game.input.keyboard.addKey(Phaser.Keyboard.D);
+
+    	$scope.up.onDown.add(function() { 
+    		$scope.direction = $scope.direction != "down" ? "up" : $scope.direction; // Cannot go from down to up
+    	}, this);
+
+    	$scope.down.onDown.add(function() { 
+    		$scope.direction = $scope.direction != "up" ? "down" : $scope.direction // Cannot go from up to down
+    	}, this);
+
+    	$scope.left.onDown.add(function() { 
+    		$scope.direction = $scope.direction != "right" ? "left" : $scope.direction // Cannot go from right to left
+    	}, this);
+
+    	$scope.right.onDown.add(function() { 
+    		$scope.direction = $scope.direction != "left" ? "right" : $scope.direction // Cannot go from right to left
+    	}, this);
+
 	},
 
 	$scope.update = function() {
-		//console.log('update');
+		console.log('update');
+		if ($scope.game.time.now - $scope.timeCheck > 100) {
+
+			$scope.timeCheck = $scope.game.time.now;
+
+			if($scope.direction == "up") {
+				$scope.position.y -= $scope.bodySize;
+			}
+			else if($scope.direction == "down") {
+				$scope.position.y += $scope.bodySize;
+				
+			}
+			else if($scope.direction == "left") {
+				$scope.position.x -= $scope.bodySize;
+			}
+			else if($scope.direction == "right") {
+				$scope.position.x += $scope.bodySize;
+			}
+
+			$scope.bodies.push(new Phaser.Rectangle($scope.position.x, $scope.position.y, $scope.bodySize, $scope.bodySize));
+		}
+
 	}
 
 	$scope.render = function() {
